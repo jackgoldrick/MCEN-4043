5edf8745250cf71941319f89190a5cc52735c710
jackgoldrick <jago5326@colorado.edu>
2024-02-04 18:27:21 -0700

    All the plots show up on one organized and labeled figure

--- l2/src/lab2.m
+++ l2/src/lab2.m
@@ -5,24 +5,72 @@
  C = 3.3e-6;
  k = 1/(R*C);
  
- t = sym('t');
+ %t = sym('t');
 
 
  %% Base
 
 vin_t = linspace(time_start,time_final,t_step);
-z = heaviside(vin_t - .000001);
-%freq = 1*2*pi;
-vin =  z;
+vin =  heaviside(vin_t - .00000001);
 Tspan = [time_start time_final];
+
+theor_vin = (1 - exp((- vin_t) / (R * C) ));
 
 IC = 0;
 
 options=odeset('RelTol',1e-4);
 
 [vout_t,vout]=ode45(@(t,y)ode_RC(t,y,vin_t,vin),Tspan,IC,options);
+%% experimental data construction
+file = "../res/scope_0.csv";
+opts = detectImportOptions(file);
 
-figure(1);
-plot(vout_t, vout)
-title("Vout v. Time base");
+data = readmatrix(file, opts);
+
+t_ex = data(:, 4) - .314;
+
+vin_ex = data(:, 5);
+
+vout_ex = data(:, 6);
+%preview(file,opts);
+
+
+%% plot 
+figure(1)
+
+hold on
+
+title("Voltage v. Time")
+xlabel('Seconds')
+ylabel('Volts')
+xlim([-.1 .8])
+ylim([ -.1 1.2])
+
+
+%%% Simulation
+ps = plot(vout_t, vout,"-.",'LineWidth',3);
+
+
+%%% Theoretical
+
+pt = plot(vin_t, theor_vin, ":",'LineWidth',3);
+
+
+
+%%% Experimental in
+
+p_ex_in = plot(t_ex, vin_ex, "-",'LineWidth',1);
+
+
+
+%%% Experimental out'
+
+p_ex_out = plot(t_ex, vout_ex, "--", 'LineWidth', 3);
+
+
+legend('Simulation', 'Theoretical', 'Experimental In', 'Experimental Out');
+
+
+hold off
+
 
