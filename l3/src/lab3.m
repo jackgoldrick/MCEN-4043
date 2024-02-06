26f377c9f720e32c1d449a0fe9e8e2b32f1c054e
jackgoldrick <jago5326@colorado.edu>
2024-02-06 14:52:11 -0700

    data added and plotted, separation and normalization needed

--- l3/src/lab3.m
+++ l3/src/lab3.m
@@ -20,20 +20,76 @@ IC = 0;
 
 options=odeset('RelTol',1e-4);
 
-[vout_t,vout]=ode45(@(t,y)ode_RC(t,y,vin_t,vin),Tspan,IC,options);
-%% experimental data construction
-% file = "../res/scope_0.csv";
-% opts = detectImportOptions(file);
-% 
-% data = readmatrix(file, opts);
-% 
-% t_ex = data(:, 4) - .314;
-% 
-% vin_ex = data(:, 5);
-% 
-% vout_ex = data(:, 6);
+% [vout_t,vout]=ode45(@(t,y)ode_RC(t,y,vin_t,vin),Tspan,IC,options);
+%% experimental 1 data construction
+file1 = "../input-data/scope_7.csv";
+
+opts1 = detectImportOptions(file1);
+
+data1 = readmatrix(file1, opts1);
+
+t_ex_1 = data1(:, 1) - .314;
+
+vin_ex_1 = data1(:, 2);
+
+vout_ex_1 = data1(:, 3);
+
+%% experimental 2 data construction
+
+file2 = "../input-data/scope_8.csv";
+
+opts2 = detectImportOptions(file2);
+
+data2 = readmatrix(file2, opts2);
+
+t_ex_2 = data2(:, 1);
+
+vin_ex_2 = data2(:, 2);
+
+vout_ex_2 = data2(:, 3);
 
 
+%% experimental 3 data construction
+
+file3 = "../input-data/scope_9.csv";
+
+opts3 = detectImportOptions(file3);
+
+data3 = readmatrix(file3, opts3);
+
+t_ex_3 = data3(:, 1);
+
+vin_ex_3 = data3(:, 2);
+
+vout_ex_3 = data3(:, 3);
+
+%% experimental 4 data construction
+
+file4 = "../input-data/scope_10.csv";
+
+opts4 = detectImportOptions(file4);
+
+data4 = readmatrix(file4, opts4);
+
+t_ex_4 = data4(:, 1);
+
+vin_ex_4 = data4(:, 2);
+
+vout_ex_4 = data4(:, 3);
+
+%% experimental 5 data construction
+
+file5 = "../input-data/scope_11.csv";
+
+opts5 = detectImportOptions(file5);
+
+data5 = readmatrix(file5, opts5);
+
+t_ex_5 = data5(:, 1);
+
+vin_ex_5 = data5(:, 2);
+
+vout_ex_5 = data5(:, 3);
 
 %% plot 
 clf
@@ -56,20 +112,54 @@ ps = plot(h_t, h,"-.",'LineWidth',3);
 
 pt = plot(vin_t, theor_vout, ":",'LineWidth',3);
 
+hold off
+
+figure(2);
+hold on
+
+title("Experimental Voltage v. Time")
+xlabel('Seconds')
+ylabel('Volts')
+xlim([-.1 .05])
+ylim([ -.1 1.1])
+
+%% Experimental in
+
+    % One
+        p_ex_in_1 = plot(t_ex_1, vin_ex_1, "-",'LineWidth',1);
+
+    % Two
+        p_ex_in_2 = plot(t_ex_2, vin_ex_2, "-",'LineWidth',1);
+
+    % Three 
+        p_ex_in_3 = plot(t_ex_3, vin_ex_3, "-",'LineWidth',1);
+
+    % Four
+        p_ex_in_4 = plot(t_ex_4, vin_ex_4, "-",'LineWidth',1);
+
+    % Five 
+        p_ex_in_5 = plot(t_ex_5, vin_ex_5, "-",'LineWidth',1);
 
 
-%%% Experimental in
+%% Experimental out
 
-% p_ex_in = plot(t_ex, vin_ex, "-",'LineWidth',1);
+    % One
+        p_ex_out_1 = plot(t_ex_1, vout_ex_1, "--", 'LineWidth', 3);
+
+    % Two 
+        p_ex_out_2 = plot(t_ex_2, vout_ex_2, "--", 'LineWidth', 3);
+    
+    % Three
+        p_ex_out_3 = plot(t_ex_3, vout_ex_3, "--", 'LineWidth', 3);
+
+    % Four 
+        p_ex_out_4 = plot(t_ex_4, vout_ex_4, "--", 'LineWidth', 3);
+
+    % Five
+        p_ex_out_5 = plot(t_ex_5, vout_ex_5, "--", 'LineWidth', 3);
 
 
-
-%%% Experimental out'
-
-% p_ex_out = plot(t_ex, vout_ex, "--", 'LineWidth', 3);
-
-
-legend('Simulation', 'Theoretical') % 'Experimental In', 'Experimental Out');
+% legend('Simulation', 'Theoretical') % 'Experimental In', 'Experimental Out');
 
 
 hold off
